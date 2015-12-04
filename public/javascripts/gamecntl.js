c65047f9cc7a8808c165bd52ed0205a9a0cfcca7
Nathan Lynch <nathanlynch@Nathans-MacBook-Pro.local>
2015-12-04 01:18:19 -0500

    sdf

--- public/javascripts/gamecntl.js
+++ public/javascripts/gamecntl.js
@@ -2,15 +2,26 @@ var yahtzee = angular.module('yahtzee', []);
 
 yahtzee.controller('game_cntl', function($scope, $http) {
     $scope.die = [
-        {"src":"images/1.png", "id": "0", "roll": "false"},
-        {"src":"images/1.png", "id": "1", "roll": "false"},
-        {"src":"images/1.png", "id": "2", "roll": "false"},
-        {"src":"images/1.png", "id": "3", "roll": "false"},
-        {"src":"images/1.png", "id": "4", "roll": "false"}
+        {"src":"images/none.png", "id": "0", "roll": "true"},
+        {"src":"images/none.png", "id": "1", "roll": "true"},
+        {"src":"images/none.png", "id": "2", "roll": "true"},
+        {"src":"images/none.png", "id": "3", "roll": "true"},
+        {"src":"images/none.png", "id": "4", "roll": "true"}
     ];
+    $scope.score = 0;
+    $scope.roll = 0;
+    $scope.round = 0;
 
-    $scope.rollDice = function(test) {
-        var scope = test;
+    $scope.rollDice = function() {
+        if ($scope.roll == 2 && $scope.round < 13) {
+            $scope.roll = -1;
+            $scope.round += 1;
+            $scope.deselectAll();
+            setScore();
+
+        } else if ($scope.roll == 3 && $scope.round >= 12) {
+            restart();
+        }
 
         console.log();
         $http({
@@ -21,7 +32,6 @@ yahtzee.controller('game_cntl', function($scope, $http) {
             headers: {
                 "Content-Type": "application/json"
             }
-
         }).then(function successCallback(response) {
             //$scope.die = response;
             console.log(response.data);
@@ -43,10 +53,10 @@ yahtzee.controller('game_cntl', function($scope, $http) {
         console.log($scope.die[id].roll);
         if ($scope.die[id].roll === 'true') {
             $scope.die[id].roll = 'false';
-            $('#' + id).css({'border': 'none'});
+            $('#' + id).css({'border': 'dotted yellow'});
         } else {
             $scope.die[id].roll = 'true';
-            $('#' + id).css({'border': 'dotted yellow'});
+            $('#' + id).css({'border': 'none'});
         }
         console.log($scope.die[id].roll);
     }
@@ -55,6 +65,23 @@ yahtzee.controller('game_cntl', function($scope, $http) {
         for (var i = 0; i < 5; i++) {
             $('#' + i).css({'border': 'none'});
         }
+
+        for (var i = 0; i < $scope.die.length; i++) {
+            $scope.die[i].roll = "true";
+        }
     }
 
+    function restart() {
+        $scope.score = 0;
+        $scope.round = 0;
+        $scope.roll = -1;
+    }
+
+    function setScore() {
+        for (var i = 0; i < $scope.die.length; i++) {
+            var score = $scope.die[i].src.substring(7, 8);
+            console.log(score);
+            $scope.score += parseInt(score);
+        }
+    }
 });
--- routes/test.js
+++ routes/test.js
@@ -6,10 +6,8 @@ router.post('/', function(req, res, next) {
     for (var i = 0; i < json.length; i++) {
         if (json[i].roll == 'true') {
             json[i].src = 'images/' + Math.floor((Math.random()*6)+1) + '.png';
-            json[i].roll = 'false';
         }
     }
-    console.log(json);
     res.json(json);
 });
 
